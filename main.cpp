a65c27192a6e1d862f057f34bf5a83603da52b72
SeppDietrich <nicolae.iurcu@colegiu.utm.md>
2024-06-14 03:00:26 +0300

    vAlpha

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <raylib.h>
+#include <iostream>
 
 
 #define WindowWidth 900
@@ -11,11 +12,48 @@ void CreateZero(int x, int y, int thickness , Color color){
 	DrawCircle(x, y , (WindowWidth/2)/SizeBoard-Thickness*2, BgColor);  
 	
 }
+void CreateX(int x , int y, int thickness, Color color){
+	Vector2 firstStart={x-WindowWidth/SizeBoard/2+thickness, y-WindowWidth/SizeBoard/2+thickness};
+	Vector2 firstEnd={x+WindowWidth/SizeBoard/2-thickness, y+WindowWidth/SizeBoard/2-thickness};
+	DrawSplineSegmentLinear(firstStart, firstEnd, thickness, color);
 
+	Vector2 secondStart={x-WindowWidth/SizeBoard/2+thickness, y+WindowWidth/SizeBoard/2-thickness };
+	Vector2 secondEnd={x+WindowWidth/SizeBoard/2-thickness, y-WindowWidth/SizeBoard/2+thickness};
+	DrawSplineSegmentLinear(secondStart, secondEnd, thickness, color);	
+}
+void PlayerMove(bool& XToMove, int table[][SizeBoard]){
+	int MouseX =GetMouseX();
+    int MouseY =GetMouseY(); 
+	int CellX= WindowWidth/SizeBoard/MouseX;
+	int CellY= WindowHeight/SizeBoard/MouseY;
+	//int x = (CellX* (WindowWidth/SizeBoard))- (WindowWidth/SizeBoard/2);
+	//int y =(CellY* (WindowHeight/SizeBoard))- (WindowHeight/SizeBoard/2);
+	if(XToMove){
+		table[CellX-1][CellY-1]=1;
+	}else{
+		table[CellX-1][CellY-1]=2;
+	}
+	XToMove!=XToMove;
+	
+}
+void ShowTable(int table[SizeBoard][SizeBoard]){
+	for(int i=0;i<SizeBoard;i++){
+		for(int j=0;j<SizeBoard;j++){
+			if(table[i][j]==1){
+				CreateX(i+1*(WindowWidth/SizeBoard)-(WindowWidth/SizeBoard/2), j+1*(WindowHeight/SizeBoard)-(WindowHeight/SizeBoard/2), Thickness, RED);
+				std::cout<<i+1*(WindowWidth/SizeBoard)-(WindowWidth/SizeBoard/2)<<std::endl<<j+1*(WindowHeight/SizeBoard)-(WindowHeight/SizeBoard/2)<<std::endl;
+			}else if(table[i][j]==2){
+				CreateZero(i+1*(WindowWidth/SizeBoard)-(WindowWidth/SizeBoard/2), j+1*(WindowHeight/SizeBoard)-(WindowHeight/SizeBoard/2), Thickness, BLUE);
+			}
+		}
+	}
+}
 
 int main() {
-	InitWindow(WindowWidth, WindowHeight, "File Manager");
+	InitWindow(WindowWidth, WindowHeight, "TicTacToe");
 	SetTargetFPS(60);
+	bool XToMove=true;
+	int table[SizeBoard][SizeBoard];
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(BgColor);
@@ -32,7 +70,8 @@ int main() {
 			Vector2 end={ i*cell_width, WindowHeight};
         		DrawLineBezier(start, end, Thickness,  BLACK);
 		}
-		CreateZero(450, 450, 20, RED);
+		if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT ))PlayerMove(XToMove, table);
+		ShowTable(table);
 		EndDrawing();
 	}
 	CloseWindow();
